4a83c8e16d1382ed0b050d169205a882f6e5d19c
Vladislav0504 <vladislav0504@yandex.ru>
2021-02-15 04:07:26 +0300

    Changes

--- Recipes.py
+++ Recipes.py
@@ -4,7 +4,7 @@ import json
 import re
 
 #Считывание html
-URL = 'https://www.bbcgoodfood.com/recipes/meatball-black-bean-chilli' 
+URL = input() 
 resp = urlopen(URL)
 html = resp.read().decode('utf8')
 soup = BeautifulSoup(html, 'html.parser')
@@ -35,15 +35,49 @@ nutrition_finding = soup.find('table')
 nutrition = ['' for i in range(16)]
 i = 0
 for td in nutrition_finding.find_all('td'):
-  nutrition[i] = td.text
-  i += 1
+  if td.text != 'low in' and td.text != 'high in' and td.text != '':
+  	nutrition[i] = td.text
+  	i += 1
+
+#Ингредиенты и инструкция
+recipe_finding = soup.find('div', class_="row recipe__instructions")
+ingredients = []
+steps = {}
+ind = 1
+for li in recipe_finding.find_all('li'):
+  if re.match(r'STEP ' + str(ind), li.text) == None and ind == 1:
+  	ingredients.append(li.text)
+  else:
+  	ind += 1
+  	steps['STEP ' + str(ind - 1)] = [li.text.replace('STEP ' + str(ind - 1), '')]
+
+#Дополнительно
+addition_finding = soup.find('div', class_="highlight-box__content editor-content")
+ind = False
+addition = {}
+if addition_finding != None:
+  ind = True
+  text = addition_finding.text.replace('\n', '').split('\r')
+  for i in range(0, len(text), 2):
+  	addition[text[i]] = (text[i + 1])
+  addition.pop('')
 
 #Рецепт
-Recipes = {
+Recipe = {
 'Name': recipe_name.text, 
 'Author': person_name.text,
 'Mark': mark,
 'Rating': ratings.text,
 'General description': [{'Preparation time': preparation_time, 'Cooking time': cooking_time}, {'Description': description}, general_information],
-'Nutrition: Per serving': [{nutrition[i]: nutrition[i + 1] for i in range(0, 16, 2)}]}
-print(json.dumps(Recipes).replace("\\u00a0", ' '))
+'Nutrition: Per serving': {nutrition[i]: nutrition[i + 1] for i in range(0, 16, 2)},
+'Ingredients': ingredients,
+'Method': steps}
+
+if ind == True:
+  Recipe['RECIPE TIPS'] = addition
+
+Recipe = re.sub(r'\\u00a0', ' ', json.dumps(Recipe))
+Recipe = re.sub(r'\\u00bd', '1/2', Recipe)
+Recipe = re.sub(r'\\u00bc', '1/4', Recipe)
+Recipe = re.sub(r'\\u2013', '-', Recipe)
+print(Recipe)
